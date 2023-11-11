1b82d8aa2af560458b22298fcf5d13abfe949843
rasin25725 <148531932+rasin25725@users.noreply.github.com>
2023-11-11 14:06:42 +0530

    Add files via upload

---
+++ page1.js
@@ -0,0 +1,7 @@
+function hoverNews(element) {
+    element.style.boxShadow = '0 8px 16px rgba(0, 0, 0, 0.2)';
+}
+
+function unhoverNews(element) {
+    element.style.boxShadow = '0 4px 8px rgba(0, 0, 0, 0.1)';
+}
