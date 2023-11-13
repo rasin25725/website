0b1be49cdd40dc9af65eb5d99be832adb43439aa
rasin25725 <148531932+rasin25725@users.noreply.github.com>
2023-11-13 17:32:56 +0530

    Update page1.js

--- page1.js
+++ page1.js
@@ -5,3 +5,20 @@ function hoverNews(element) {
 function unhoverNews(element) {
     element.style.boxShadow = '0 4px 8px rgba(0, 0, 0, 0.1)';
 }
+function login() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    var errorMessage = document.getElementById("errorMessage");
+
+    if (username === "admin@123" && password === "admin123") {
+    
+        // Successful login
+        errorMessage.textContent = "Login successful!";
+        errorMessage.style.color = "green";
+        window.location.href="page1.html";
+    } else {
+        // Incorrect credentials
+        errorMessage.textContent = "Password is incorrect.";
+        errorMessage.style.color = "red";
+    }
+}
